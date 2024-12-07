4442b061bfbd5a1e34d7429fb88c318ecc322d57
Jeff <jeffreysofly0320@gmail.com>
2024-12-06 21:30:05 -0500

    Added feature to work with flutter app

--- ESP32/sketch_oct24a/sketch_oct24a.ino
+++ ESP32/sketch_oct24a/sketch_oct24a.ino
@@ -1,3 +1,4 @@
+
 /*
 #include <PN5180.h>
 #include <PN5180ISO15693.h>
@@ -118,10 +119,12 @@ void loop() {
 #include <WiFi.h>
 #include "time.h"
 #include "config.h"
+#include <WebServer.h>
+#include <esp_task_wdt.h>
 
 // Wifi
 const char* ssid = "DD4A982.4";
-const char* password = WIFI_PASSWORD;
+const char* password = "ANUBIS032097!";
 
 // Define PN5180 pins
 const int nssPin = 5;     // NSS pin
@@ -140,14 +143,42 @@ int hopperServoAngle = 0;
 
 Servo hopperServo;
 
-
 // Setup NTP server
 const char* ntpServer = "pool.ntp.org";  // network time protocol
-const long gmtOffset_sec = -18000; // Eastern standard time
-const int daylightOffset_sec = 3600; 
+const long gmtOffset_sec = -18000;       // Eastern standard time
+const int daylightOffset_sec = 3600;
 
 // Initialize PN5180
 PN5180ISO14443 nfcReader(nssPin, busyPin, resetPin);
+
+WebServer server(80);
+
+/*
+void handleRoot() {
+  if (server.args() > 0) {
+    //String receivedData = server.arg("plain");
+    //Serial.println("Received Data: " + receivedData);
+    // Send an HTML page with the received data
+    String html = "<!DOCTYPE html><html><head><title>ESP32 Server</title></head><body>";
+    html += "<ul>";
+    // Iterate through all arguments
+    for (int i = 0; i < server.args(); i++) {
+      String argName = server.argName(i);                    // Get argument name
+      String argValue = server.arg(i);                       // Get argument value
+      Serial.println("Arg: " + argName + " = " + argValue);  // Print to serial monitor
+
+      // Add argument to HTML response
+      html += "<li>" + argName + ": " + argValue + "</li>";
+    }
+
+    html += "</ul>";
+    html += "<a href=\"/\">Refresh</a>";  // Add a refresh link
+    html += "</body></html>";
+    server.send(200, "text/html", html);
+  } else {
+    server.send(400, "text/plain", "No data received");
+  }
+}*/
 
 void setup() {
   Serial.begin(115200);
@@ -159,13 +190,18 @@ void setup() {
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(1000);
     Serial.print(".");
   }
   Serial.println(" Connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
   struct tm timeinfo;
+
+  //server.on("/", handleRoot);
+  server.begin();
 
   // Initialize SPI
   SPI.begin();
@@ -181,6 +217,9 @@ void setup() {
 }
 
 void loop() {
+
+  server.handleClient();
+
   uint8_t uid[10];                                    // Buffer for the UID
   uint8_t uidLength = nfcReader.readCardSerial(uid);  // Actual length of UID
   String tagID;
@@ -188,17 +227,19 @@ void loop() {
   nfcReader.reset();
   nfcReader.setupRF();
 
+  //connectServer();
+
   struct tm timeinfo;
 
   if (getLocalTime(&timeinfo)) {
-    Serial.printf("Current time: %04d-%02d-%02d %02d:%02d:%02d\n",
+    /*Serial.printf("Current time: %04d-%02d-%02d %02d:%02d:%02d\n",
                 timeinfo.tm_year + 1900,
                 timeinfo.tm_mon + 1,
                 timeinfo.tm_mday,
                 timeinfo.tm_hour,
                 timeinfo.tm_min,
-                timeinfo.tm_sec);
-    if (timeinfo.tm_hour == 8 && timeinfo.tm_min == 40 && (timeinfo.tm_sec >= 0 && timeinfo.tm_sec <= 5) && timeinfo.tm_year + 1900 == 2024 && timeinfo.tm_mon == 10 && timeinfo.tm_mday == 11) {
+                timeinfo.tm_sec);*/
+    if (timeinfo.tm_hour == 16 && timeinfo.tm_min == 29 && (timeinfo.tm_sec >= 0 && timeinfo.tm_sec <= 8) && timeinfo.tm_year + 1900 == 2024 && timeinfo.tm_mon == 10 && timeinfo.tm_mday == 11) {
       if (hopperServoAngle == 0) {
         hopperServoAngle = 90;
         delay(3000);
@@ -208,15 +249,12 @@ void loop() {
       }
       hopperServo.write(hopperServoAngle);
     } else {
-      Serial.println("Not time yet");
+      //Serial.println("Not time yet");
     }
 
   } else {
     Serial.println("Failed to obtain time");
   }
-  //Serial.println(asctime(&timeinfo));
-
-  //Serial.println(&timeinfo, "%A, %B %d %Y %I:%M:%S");
 
   // Check for an ISO14443 tag
   if (nfcReader.readCardSerial(uid)) {
@@ -225,8 +263,6 @@ void loop() {
       tagID.concat(String(uid[i], HEX));
       tagID.toUpperCase();
     }
-
-    //Serial.println(tagID);
 
     if (tagID == "3DE1490") {
       if (angle == 90) {
