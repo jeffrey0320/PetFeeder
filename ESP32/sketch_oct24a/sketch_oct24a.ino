b42652cc6e9595d7308fce1434b308ee795bd2f0
Jeff <jeffreysofly0320@gmail.com>
2024-12-09 06:58:16 -0500

    catfeeder product

--- ESP32/sketch_oct24a/sketch_oct24a.ino
+++ ESP32/sketch_oct24a/sketch_oct24a.ino
@@ -1,116 +1,3 @@
-
-/*
-#include <PN5180.h>
-#include <PN5180ISO15693.h>
-#include <SPI.h>
-
-const byte nssPin = 5;
-const byte busyPin = 16;
-const byte resetPin = 17;
-
-PN5180ISO15693 abc(5, 16, 17);
-
-uint8_t previousUid[8];
-
-//string tagID;
-//uint8_t uidLength;
-//uint8_t uid[10];
-
-void setup() {
-  
-  Serial.begin(115200);
-  SPI.begin();
-  abc.begin();
-  abc.reset();
-  uint8_t productVersion[2];
-  abc.readEEprom(PRODUCT_VERSION, productVersion, sizeof(productVersion));
-  abc.setupRF();
-
-  Serial.println(F("PN5180 WORKING!"));
-}
-
-void loop() {
-
-  uint8_t thisUid[8];
-
-  ISO15693ErrorCode rc = abc.getInventory(thisUid);
-
-  Serial.println(rc);
-  abc.reset();
-  abc.setupRF();
-
-  if (rc == ISO15693_EC_OK) {
-    Serial.print(F("ID: "));
-    for (int j = 0; j < sizeof(thisUid); j++) {
-      Serial.print(thisUid[j], HEX);
-      Serial.print(" ");
-    }
-    Serial.println();
-  }else {
-    Serial.println(F("No tag detected or an error occured."));
-  }
-  delay(1000);
-  /*
-  if(!abc.isCardPresent()){
-    return;
-  }
-  uidLength = abc.readCardSerial(uid);
-  Serial.println(uidLength);
-  if (memcmp(uid, previousUid, uidLength) == 0) {
-    return;
-  }
-  if (uidLength > 0) {
-    Serial.print(abc.readCardSerial(uid));
-    printHEX(uid, uidLength);
-    // Update the previous UID
-    memcpy(previousUid, uid, uidLength);
-    return;
-  }
-}*/
-
-/*
-#include <PN5180ISO15693.h>
-
-const byte nssPin = 5;      // Chip Select (NSS) pin
-const byte busyPin = 16;    // BUSY pin
-const byte resetPin = 17;   // RESET pin
-
-PN5180ISO15693 nfcReader(nssPin, busyPin, resetPin);
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting PN5180 Reader...");
-
-  // Initialize the PN5180 module
-  nfcReader.begin();  
-  nfcReader.reset();
-  
-  nfcReader.setupRF();  // Set up RF configurations
-}
-
-void loop() {
-  Serial.println("Scanning for tag...");
-
-  uint8_t uid[8];  // Buffer for UID (Unique Identifier)
-  ISO15693ErrorCode result = nfcReader.getInventory(uid);  // Attempt to read tag
-
-  nfcReader.reset();
-  nfcReader.setupRF();
-
-  if (result == ISO15693_EC_OK) {  // Check if a tag was successfully read
-    Serial.print("Tag detected with UID: ");
-    for (int i = 0; i < sizeof(uid); i++) {
-      Serial.print(uid[i], HEX);  // Print each byte of the UID in HEX
-      Serial.print(" ");
-    }
-    Serial.println();
-  } else {
-    Serial.println("No tag detected.");
-  }
-
-  delay(1000);  // Wait a bit before trying again
-}*/
-
 // ISO 14443
 #include <PN5180.h>
 #include <PN5180ISO14443.h>
@@ -120,11 +7,13 @@ void loop() {
 #include "time.h"
 #include "config.h"
 #include <WebServer.h>
-#include <esp_task_wdt.h>
+#include <map>
+#include <string>
+using namespace std;
 
 // Wifi
 const char* ssid = "DD4A982.4";
-const char* password = "ANUBIS032097!";
+const char* password = WIFI_PASSWORD;
 
 // Define PN5180 pins
 const int nssPin = 5;     // NSS pin
@@ -153,32 +42,45 @@ PN5180ISO14443 nfcReader(nssPin, busyPin, resetPin);
 
 WebServer server(80);
 
-/*
+struct mealTime {
+  int hour;
+  int minute;
+};
+
+std::map<String, mealTime> mealTimes;
+mealTime breakfastTime = mealTimes["breakfast"];
+mealTime lunchTime = mealTimes["lunch"];
+mealTime dinnerTime = mealTimes["dinner"];
+
+mealTime parseTime(String time) {
+  int indexOfColon = time.indexOf(':');
+  int hour = time.substring(0, indexOfColon).toInt();
+  int minute = time.substring(indexOfColon + 1).toInt();
+  return { hour, minute };
+}
+
 void handleRoot() {
+
   if (server.args() > 0) {
-    //String receivedData = server.arg("plain");
-    //Serial.println("Received Data: " + receivedData);
-    // Send an HTML page with the received data
-    String html = "<!DOCTYPE html><html><head><title>ESP32 Server</title></head><body>";
-    html += "<ul>";
     // Iterate through all arguments
     for (int i = 0; i < server.args(); i++) {
-      String argName = server.argName(i);                    // Get argument name
-      String argValue = server.arg(i);                       // Get argument value
-      Serial.println("Arg: " + argName + " = " + argValue);  // Print to serial monitor
+      String argName = server.argName(i);
+      String argValue = server.arg(i);
 
-      // Add argument to HTML response
-      html += "<li>" + argName + ": " + argValue + "</li>";
+      mealTime timeOfMeal = parseTime(argValue);
+      mealTimes[argName] = timeOfMeal;
+
+      breakfastTime = mealTimes["breakfast"];
+      lunchTime = mealTimes["lunch"];
+      dinnerTime = mealTimes["dinner"];
     }
 
-    html += "</ul>";
-    html += "<a href=\"/\">Refresh</a>";  // Add a refresh link
-    html += "</body></html>";
-    server.send(200, "text/html", html);
+    printMealTimes();
+
   } else {
     server.send(400, "text/plain", "No data received");
   }
-}*/
+}
 
 void setup() {
   Serial.begin(115200);
@@ -200,8 +102,10 @@ void setup() {
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
   struct tm timeinfo;
 
-  //server.on("/", handleRoot);
+  server.on("/", handleRoot);
   server.begin();
+
+  printMealTimes();
 
   // Initialize SPI
   SPI.begin();
@@ -231,48 +135,64 @@ void loop() {
 
   struct tm timeinfo;
 
-  if (getLocalTime(&timeinfo)) {
-    /*Serial.printf("Current time: %04d-%02d-%02d %02d:%02d:%02d\n",
-                timeinfo.tm_year + 1900,
-                timeinfo.tm_mon + 1,
-                timeinfo.tm_mday,
-                timeinfo.tm_hour,
-                timeinfo.tm_min,
-                timeinfo.tm_sec);*/
-    if (timeinfo.tm_hour == 16 && timeinfo.tm_min == 29 && (timeinfo.tm_sec >= 0 && timeinfo.tm_sec <= 8) && timeinfo.tm_year + 1900 == 2024 && timeinfo.tm_mon == 10 && timeinfo.tm_mday == 11) {
-      if (hopperServoAngle == 0) {
-        hopperServoAngle = 90;
-        delay(3000);
-      } else {
-        hopperServoAngle = 0;
-        delay(1000);
-      }
-      hopperServo.write(hopperServoAngle);
-    } else {
-      //Serial.println("Not time yet");
-    }
+  time_t now = time(nullptr);
+  struct tm* current = localtime(&now);
 
-  } else {
+  if (getLocalTime(&timeinfo)) {
+    for (auto const& m : mealTimes) {
+      const String& mealName = m.first;
+      const mealTime& mealTime = m.second;
+
+      if(timeinfo.tm_hour == mealTime.hour && timeinfo.tm_min == mealTime.minute && (timeinfo.tm_sec >= 0 && timeinfo.tm_sec <= 8)){
+        if (hopperServoAngle == 0) {
+          hopperServoAngle = 90;
+          delay(3000);
+        } else {
+          hopperServoAngle = 0;
+          delay(1000);
+        }
+        hopperServo.write(hopperServoAngle);
+      }else {
+        //Serial.println("Not time yet");
+      }
+    }
+  }else {
     Serial.println("Failed to obtain time");
   }
 
-  // Check for an ISO14443 tag
-  if (nfcReader.readCardSerial(uid)) {
+    // Check for an ISO14443 tag
+    if (nfcReader.readCardSerial(uid)) {
 
-    for (int i = 0; i < uidLength; i++) {
-      tagID.concat(String(uid[i], HEX));
-      tagID.toUpperCase();
-    }
-
-    if (tagID == "3DE1490") {
-      if (angle == 90) {
-        angle = 0;
-      } else {
-        angle = 90;
+      for (int i = 0; i < uidLength; i++) {
+        tagID.concat(String(uid[i], HEX));
+        tagID.toUpperCase();
       }
-      servo.write(angle);
+
+      if (tagID == "3DE1490") {
+        if (angle == 90) {
+          angle = 0;
+        } else {
+          angle = 90;
+        }
+        servo.write(angle);
+      }
     }
+
+    delay(1000);  // Wait for 1 second before scanning again
   }
 
-  delay(1000);  // Wait for 1 second before scanning again
-}
+  void printMealTimes() {
+    Serial.print("Current meal times: ");
+    for (auto const& m : mealTimes) {
+      const String& mealName = m.first;
+      const mealTime& time = m.second;
+      Serial.print(time.hour);
+      Serial.print(':');
+      if (time.minute < 10) {
+        Serial.print('0');
+      }
+      Serial.print(time.minute);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
